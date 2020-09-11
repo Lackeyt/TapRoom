ea229047947bbc82d6e64567fdbeadaf063ce09a
Tyson Lackey <lackeyt90@gmail.com>
2020-09-11 11:59:37 -0700

    update keg, keg deatils, keg list

---
+++ src/components/KegDetail.js
@@ -0,0 +1,27 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function KegDetail(props){
+  const {keg, onClickingDelete} = props;
+
+  return(
+    <React.Fragment>
+      <h1>Keg Details</h1>
+      <h3>{keg.name} - {keg.brand}</h3>
+      <p>{keg.price}</p>
+      <p><em>{keg.alcoholContent}</em></p>
+      <p>Pints Remaining: </p>
+      <button onClick={props.onClickingEdit}>Update Keg info</button>
+      <button onClick={()=>onClickingDelete(keg.id)}>Remove Keg</button>
+      <hr/>
+    </React.Fragment>
+  );
+}
+
+KegDetail.PropTypes = {
+  keg: PropTypes.object,
+  onClickingDelete: PropTypes.func,
+  onClickingEdit: PropTypes.func
+}
+
+export default KegDetail;
